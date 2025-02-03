320ccd0b1b23b28b65b6a87517577dcd9d8c879e
vithyawessalini <vithyawessalini@gmail.com>
2025-02-03 16:04:58 +0530

    Updated portfolio

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/portfolio" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio/about" element={<About />} />
+          <Route path="/portfolio/skills" element={<Skills />} />
+          <Route path="/portfolio/projects" element={<Projects />} />
+          <Route path="/portfolio/contact" element={<Contact />} />
         </Routes>
         <CustomCursor />
       </div>
