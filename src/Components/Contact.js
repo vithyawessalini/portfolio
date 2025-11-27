ba0c35022a1f04b5d4a3ac2fc8b43acfb5fa0263
vithyawessalini <118716389+vithyawessalini@users.noreply.github.com>
2025-11-27 21:40:27 +0530

    Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,7 +7,6 @@ const Contact = () => {
       <h2>Contact Me</h2>
       <p>If you have any questions or want to get in touch, feel free to contact me at:</p>
       <p>Email: <a href="mailto:vithyawessalini@gmail.com">vithyawessalini@gmail.com</a></p>
-      <p>Phone: <a href="tel:8056987476">8056987476</a></p>
       <p>Connect with me on:</p>
       <ul>
         <li><a href="https://linkedin.com/in/vithyawessalini-a-p" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
